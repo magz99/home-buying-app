ec81d2e559f7900c1e38860f05600b3376362397
magz99 <magzb.99@gmail.com>
2020-11-01 17:38:56 -0500

    Adding Tailwind css, header links

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li><a href="/financial-information">Financial Information</a></li>
+            <li><a href="/mortgage-calculation">Mortgage Calculation</a></li>
+            <li><a href="/property-map">Property Map</a></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
